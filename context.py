2b4d7a3d57a1de3ee79acd85ae664523d3e83c39
DESKTOP-64SNP04\Juntae Kim <deluxe9926@gmail.com>
2021-11-07 22:29:47 +0900

    using 1d gaussian kernel

--- context.py
+++ context.py
@@ -1,20 +1,11 @@
 import json
-import os
 from collections import deque
 import numpy as np
+from math import pi, sqrt, exp
 
-def frameScore(json_dir, label_score, iteration):
-    with open(json_dir) as f:
-        json_data = json.load(f)
-
-    frame_score = label_score
-    for j in range(0, 5):
-        top = json_data[iteration]["frame_result"][j]["label"]["description"]
-        score = round(json_data[iteration]["frame_result"][j]["label"]["score"], 2)
-        frame_score[top] = score
-    return frame_score
 
 def labelMap(label_map_path):
+    # {0:amusementpark, 1:aquarium ```}
     label_map = {}
     with open(label_map_path, 'r', encoding='utf-8') as t:
         for i, line in enumerate(t.readlines()):
@@ -22,7 +13,9 @@ def labelMap(label_map_path):
             label_map[i] = line
     return label_map
 
+
 def labelScore(label_map_path):
+    # Initialize scores to zero {amusementpark:0, aquarium:0 ```}
     label_score = {}
     with open(label_map_path, 'r', encoding='utf-8') as t:
         for i, line in enumerate(t.readlines()):
@@ -31,87 +24,100 @@ def labelScore(label_map_path):
     return label_score
 
 
-def placeContext(base_dir, VIDEO_NAME):
-    with open(base_dir + '/smoothing_{}.json'.format(VIDEO_NAME)) as f:
-        json_data = json.load(f)
+class gaussianGrouping:
+    def __init__(self, frame_results, label_map_path, video_fps=30, window_size=3):
+        self.frame_results = frame_results
+        self.label_map_path = label_map_path
+        self.video_fps = video_fps
+        self.window_size = window_size
+        self.sigma = 1.5
 
-    data = []
-    file_list = []
-    result = None
+    def makeGaussian(self):
+        n = self.window_size * 2 + 1
+        r = range(-int(n / 2), int(n / 2) + 1)
+        weight = [1 / (self.sigma * sqrt(2 * pi)) * exp(-float(x) ** 2 / (2 * self.sigma ** 2)) for x in r]
+        weight = np.reshape(weight, (n, 1))  # Adding dimension
+        return weight
 
-    for i in range(len(json_data)-1):
-        if json_data[i]["description"] == json_data[i+1]["description"]:
-            file_list.append(i)
-            description = json_data[i]["description"]
-            result = {"file_context": file_list, "description": description}
-            if i == len(json_data)-2:
-                data.append(result)
+    def frameScore(self):
+        # entering the real scores {aquarium:0.5, aquarium:0.2 ```}
+        label_score = labelScore(label_map_path=self.label_map_path)
+        frame_scores = []
+        for i in range(len(self.frame_results)):
+            for result in self.frame_results[0]['frame_result']:
+                frame_score = label_score
+                top = result["label"]["description"]
+                score = round(result["label"]["score"], 2)
+                frame_score[top] = score
+                frame_scores.append(frame_score)
 
-        else:
-            file_list.append(i)
-            description = json_data[i]["description"]
-            result = {"file_context": file_list, "description": description}
-            data.append(result)
-            file_list = []
-            result = {}
-    with open(base_dir + '/smoothing_context_{}.json'.format(VIDEO_NAME), 'w') as outfile:
-        json.dump(data, outfile, indent='\t')
+        return frame_scores
 
-def main():
-    file_dirs = []
-    file_names = []
-    sevenKeys = []
-    label_map = labelMap('/workspace/classes.txt')
-    weight = [[0.1], [0.1], [0.15], [0.3], [0.15], [0.1], [0.1]]
-    weight_2 = [[0.05], [0.075], [0.2], [0.35], [0.2], [0.075], [0.05]]
-    weight_3 = [[0.05], [0.25], [0.4], [0.25], [0.05]]
-
-    for i in os.listdir("/workspace/jt/places/inference_frame_211008/"):
-        file_dirs.append("/workspace/jt/places/inference_frame_211008/" + i)
-        file_names.append(i.split('/')[-1])
-        print(file_names)
-
-    for i in range(len(file_dirs)):
-        VIDEO_NAME = file_names[i]
-        print(VIDEO_NAME + " is working")
-        base_dir = "/workspace/jt/places/inference_frame_211008/{}/".format(VIDEO_NAME)
-
-        # IMAGE
-        img_dir = base_dir + "frame{}/".format(VIDEO_NAME)
-        img_list = [_ for _ in os.listdir(img_dir) if _.endswith(r".jpg")]
+    def placesContext(self):
+        weight = self.makeGaussian()
+        label_map = labelMap(self.label_map_path)
+        frame_scores = self.frameScore()
+        inference_list = []
         dq = deque([])
 
-        data = []
-        inference_list = [0, 0, 0]
+        for i in range(len(frame_scores)):
+            if i < self.window_size or i >= len(frame_scores) - self.window_size:
+                a = max(frame_scores[i].keys())
+                inference_list.append(a)
 
-        for i in range(len(img_list)):
-            json_dir = base_dir + "{}.json".format(VIDEO_NAME)
-            label_score = labelScore('/workspace/classes.txt')
-
-            frame_score = frameScore(json_dir, label_score, i)
-            dq.append(frame_score)
-
+            dq.append(frame_scores[i])
             if len(dq) == len(weight):
-                for i in dq:
-                    arr1 = list(map(float, (k for k in i.values())))
-                    sevenKeys.append(arr1)
-                sevenKeys = np.array(sevenKeys)
-                sevenKeys.reshape((len(dq), 16))
-                mul = np.multiply(sevenKeys, weight)
-                mul = np.add.reduce(mul, axis=0)
+                sumMatrix = []
+                for j in dq:
+                    arr1 = list(map(float, (k for k in j.values())))
+                    sumMatrix.append(arr1)
+                sumMatrix = np.array(sumMatrix).reshape((len(dq), 16))
+                mul = np.add.reduce(np.multiply(sumMatrix, weight), axis=0)
                 max_index = np.argmax(mul)
                 inference_list.append(label_map[max_index])
-                sevenKeys = []
                 dq.popleft()
+        return inference_list
 
+    def smoothing(self):
+        inference_list = self.placesContext()
+        result = []
+        file_list = []
         for i in range(len(inference_list)):
-            result = {"file_number": i, "description": inference_list[i]}
-            data.append(result)
+            if i == 0:
+                file_list.append(inference_list[i])
+                continue
 
-        with open(base_dir + '/smoothing_{}.json'.format(VIDEO_NAME), 'w') as outfile:
-            json.dump(data, outfile, indent='\t')
-        placeContext(base_dir, VIDEO_NAME)
-        print(data)
+            if inference_list[i] == inference_list[i - 1]:
+                file_list.append(inference_list[i])
+
+            else:
+                result.append(file_list)
+                file_list = []
+                file_list.append(inference_list[i])
+        print(result)
+
+        sequence_result = []
+        countLength = 0
+        for i in range(len(result)):
+            description = result[i][0]
+            start_frame = countLength * self.video_fps
+            print('start frame', start_frame)
+            length = len(result[i])
+            countLength += length
+            end_frame = (countLength - 1) * self.video_fps
+            print(end_frame)
+            frame_result = {"start_frame": start_frame, "end_frame": end_frame, "label": {}}
+            label = {"description": description}
+            frame_result["label"] = label
+            sequence_result.append(frame_result)
+        return sequence_result
+
 
 if __name__ == '__main__':
-    main()
+    VIDEO_NAME = 'koreanhouse_02'
+    base_dir = "/workspace/jt/places/inference_frame_211003/{}".format(VIDEO_NAME)
+    with open(base_dir + '/{}.json'.format(VIDEO_NAME)) as f:
+        json_data = json.load(f)
+    f = gaussianGrouping(frame_results=json_data, label_map_path='/workspace/classes.txt')
+    sequence_result = f.smoothing()
+    print(sequence_result)
